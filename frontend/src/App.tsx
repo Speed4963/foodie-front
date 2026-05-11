2de2a08f799e755bcad5cbdcef1e35774d2d9df1
gksalfus77-gif <gksalfus77@gmail.com>
2026-05-11 17:20:46 +0900

    변경
    변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,11 @@
-// ============================================================
-// App.tsx — 잇픽 (EAT PICK)
-// 페이지: home | map | blog
-// ============================================================
-import { useState } from 'react'
-import './index.css'
-import MainPage from './pages/MainPage'
-import MapPage from './pages/MapPage'
-import BlogPage from './pages/BlogPage'
-import Layout from './components/Layout'
-
-export type PageType = 'home' | 'map' | 'blog'
+import { RouterProvider } from "react-router-dom";
+import router from "./Routers/Router";
+import "./App.css";
 
 function App() {
-  const [page, setPage] = useState<PageType>('home')
-
-  return (
-    <Layout currentPage={page} onNavigate={setPage}>
-      {page === 'home' && <MainPage onNavigate={setPage} />}
-      {page === 'map'  && <MapPage />}
-      {page === 'blog' && <BlogPage />}
-    </Layout>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
+
+
--- frontend/src/Routers/Router.tsx
+++ frontend/src/Routers/Router.tsx
@@ -1,24 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../pages/Main";
 import Layout from "../components/Layout";
-import type { PageType } from "../App";
-
 
 const router = createBrowserRouter([
-  // ✅ Door - 첫 진입 페이지 (navbar 없음)
+  // 첫 페이지
   {
     path: "/",
     element: <Main />,
   },
 
-  // ✅ Layout(navbar) 있는 페이지들
+  // navbar 포함 페이지
   {
     path: "/main",
-    element: <Layout currentPage={"map"} onNavigate={function (page: PageType): void {
-      throw new Error("Function not implemented.");
-    } } children={undefined} />,
+    element: < Main/>,
     children: [
-     
+      // 예시
+      // {
+      //   path: "map",
+      //   element: <MapPage />,
+      // },
     ],
   },
 ]);
