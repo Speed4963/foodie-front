9a5a7ed61fe458009df42df3f5c48b357bdeaf90
rongguo97 <rongguo97@gmail.com>
2026-05-26 18:07:47 +0900

    업데이트

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from 'react-router-dom'
-import router from '../src/Routers/Router' // 👈 질문자님의 router.tsx 파일 경로로 수정하세요!
-import { AuthProvider } from './context/AuthContext'; // 1. 방금 만든 AuthProvider 불러오기
+import router from '../src/Routers/Router' 
+import { AuthProvider } from './contexts/AuthContext';
 // ✅ 이렇게 되어 있어야 해요
 export type PageType = 'home' | 'map' | 'blog'
 
